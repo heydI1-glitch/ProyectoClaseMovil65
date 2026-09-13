842e9185327d9e02cb64a0e73ba5cee53e68503f
heydI1-glitch <heydimolina019@gmail.com>
2026-09-12 20:31:20 -0600

    registrado skincareReducer bajo la clave skincare

--- MyRoutineApp/src/store/index.ts
+++ MyRoutineApp/src/store/index.ts
@@ -1,11 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userProfileReducer from "./slices/userProfileSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import userProfileReducer from './slices/userProfileSlice';
+import skincareReducer from './slices/skincareSlice';
 
-export const store = configureStore ({
-    reducer: {
-        userProfile: userProfileReducer
-    }, 
+export const store = configureStore({
+  reducer: {
+    userProfile: userProfileReducer,
+    skincare: skincareReducer,
+  },
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
